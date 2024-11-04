59b3539c8937d6790022037266dda58324d47499
Polina <prrshka@mail.ru>
2024-11-05 02:49:27 +0300

    gotovo zadanie

---
+++ pythonProject1/main.py
@@ -0,0 +1,18 @@
+#1st program
+a = 9**0.5*5
+print(a)
+
+#2nd program
+b = (9.99 > 9.98) and (1000 != 1000.1)
+print(b)
+
+#3rd program
+i = 2*2+2
+i1 = 2*(2+2)
+print(i==i1)
+
+#4th program
+s = '123.456'
+k = float(s)*10
+c = int(k)%10
+print(c)
