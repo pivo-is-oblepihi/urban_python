c787debc6d72d96687709d4c75db8ea494f04945
Polina <prrshka@mail.ru>
2024-11-05 13:23:23 +0300

    add file les 2

---
+++ modul 1/les2_peremen.py
@@ -0,0 +1,6 @@
+quantity_dz = 12
+quantity_haur = 1.5
+name_course = 'Python'
+execution_speed = quantity_haur/quantity_dz
+print('Курс:', name_course, ', всего задач:', quantity_dz, ', затрачено часов', quantity_haur,
+      ', среднее время выполнения', execution_speed, 'часа')
