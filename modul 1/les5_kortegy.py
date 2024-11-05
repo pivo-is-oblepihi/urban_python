9fbce12a07597ff1727a69328dc95ffb7d134cd6
Polina <prrshka@mail.ru>
2024-11-05 14:57:16 +0300

    add file les 5

---
+++ modul 1/les5_kortegy.py
@@ -0,0 +1,8 @@
+immutable_var = 1, [2,3,4], 'a', 'b'
+print(immutable_var)
+#immutable_var[0] = 200
+#print(immutable_var)
+
+mutable_list = [1, 2, 34, 15, 89]
+mutable_list[0] = 200
+print(mutable_list)
