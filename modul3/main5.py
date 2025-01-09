a2171987ab3911a39e4c9d4e218fb145891c8a58
polina-is-oblepihi <prrshka@mail.ru>
2025-01-09 15:45:15 +0300

    add task 5 unfinished

--- modul3/main5.py
+++ modul3/main5.py
@@ -1,16 +1,15 @@
-def get_multiplied_digits(str_number):
+def get_multiplied_digits(str_number : int):
+    str_number = str(str_number)
     first = int(str_number[0])
-    if first == 0:
-        t = int(str_number[1:])
-        first = first + t
-    elif len(str_number) <= 1:
+    if first > 1:
+        return first * int(get_multiplied_digits(str_number[1:]))
+    elif first != 0:
         return first
     else:
-        t = int(get_multiplied_digits(str_number[1:]))
-        return first * t
+        return 1
 
 
-result = get_multiplied_digits('40203')
+result = get_multiplied_digits(40203)
 print(result)
-result2 = get_multiplied_digits('402030')
+result2 = get_multiplied_digits(402030)
 print(result2)
