a7ba7aa8492998e7ed5a201d2031d41611db44bf
polina-is-oblepihi <prrshka@mail.ru>
2025-01-16 13:25:05 +0300

    add mod task3

--- modul5/class_house_2.py
+++ modul5/class_house_2.py
@@ -10,6 +10,14 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if self.new_floor > self.number_of_floors or self.new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, self.new_floor):
+                print(i)
+                
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 
